b330d56935220403c94c5dce421edeed30318b32
dearzd <1147433303@qq.com>
2020-04-22 22:53:51 +0800

    number regular expresstion fix defect

--- week02/nubmerRegExp.js
+++ week02/nubmerRegExp.js
@@ -9,7 +9,7 @@
 // NaN
 
 function testNumber(str) {
-    const reg = /(^(\+|-|\.)?[0-9]*((\.[0-9]*|e-?\+?[0-9]+)|(\.[0-9]*[0-9]+e-?\+?[0-9]+))?$)|(^0x([0-9]+|[a-f]+|[A-F]+)$)|(^0b([0-1]+)$)|(^0o([0-7]+)$)|Infinity|NaN/;
+    const reg = /(^(\+|-|\.)?[0-9]*((\.[0-9]*|e-?\+?[0-9]+)|(\.[0-9]*[0-9]+e-?\+?[0-9]+))?$)|(^0(x|X)([0-9]+|[a-f]+|[A-F]+)$)|(^0(b|B)([0-1]+)$)|(^0(o|O)([0-7]+)$)|Infinity|NaN/;
     return reg.test(str);
 }
 
@@ -39,12 +39,15 @@ expect('1.7976931348623157e+308', true);
 expect('12ee3', false);
 expect('12e33e4', false);
 expect('0x10', true);
+expect('0X10', true);
 expect('0xF', true);
 expect('0xf', true);
 expect('0xg', false);
 expect('0b10', true);
+expect('0B10', true);
 expect('0b2', false);
 expect('0o10', true);
+expect('0O10', true);
 expect('0o7', true);
 expect('0o8', false);
 expect('Infinity', true);
